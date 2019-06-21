9e626bef0ac5b22c1531874786ec1ce703a5687e
Jhana Gustave <jhana.gustave@gmail.com>
2019-06-21 20:02:51 +0000

    Automatically backed up by Learn

--- app_controller/controller.rb
+++ app_controller/controller.rb
@@ -9,8 +9,16 @@ class ApplicationController < Sinatra::Base
   	erb :index
   end
   
-  get "/hello" do
-   erb :hello
+  get '/hello' do 
+    erb :hello
+  end
+  
+  get '/goodbye' do 
+    erb :goodbye
+  end
+  
+  get '/date' do 
+    erb :date
   end
   
 end
